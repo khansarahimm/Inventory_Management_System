bc47b30fc96adb2e4ea58281f7529941ea5bb73c
Khansarahimm <khansahashmi01060@gmail.com>
2025-03-18 19:43:56 +0500

    UPdated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
+
+
 export default defineConfig({
     plugins: [
         laravel({
